e2b7d4d1b4fd0a200fbfc3359a429d82f0d35e00
robind <robind@jfrog.com>
2023-08-04 00:01:48 +0530

    test

--- namespace-cleanup/cleanup_ns.py
+++ namespace-cleanup/cleanup_ns.py
@@ -1,23 +1,45 @@
-pipelines:
-  - name: clean_old_namespaces
-    steps:
-      - name: run_python_cleanup_script
-        type: Bash
-        configuration:
-          environmentVariables:
-            age_to_delete:
-              default: 10
-              description: Deletes namespaces older than age_to_delete
-              allowCustom: true
-            name_regex:
-              default: "a*"
-              description: Deletes namespaces older matching the regex provided
-              allowCustom: true
-          integrations:
-            - name: test_kubernetes
-        execution:
-          onExecute:
-            - "cd namespace-cleanup && pip3 install -r requirements.txt"
-            - "python3 ../cleanup_ns.py $age_to_delete $name_regex"
+import argparse
+import datetime
+import pytz
+import re
+from kubernetes import client, config
 
+def delete_old_namespaces(client, days, name_regex):
+    v1 = client.CoreV1Api()
+    current_time = datetime.datetime.now(pytz.UTC)
 
+    # Get all namespaces
+    namespaces = v1.list_namespace().items
+
+    for namespace in namespaces:
+        namespace_creation_time = namespace.metadata.creation_timestamp
+        age = current_time - namespace_creation_time
+
+        # Check if the namespace name matches the regex pattern
+        if name_regex.match(namespace.metadata.name):
+            # Check if the namespace is older than the specified number of days
+            if age.days >= days:
+                print(f"Deleting namespace: {namespace.metadata.name}")
+                v1.delete_namespace(namespace.metadata.name)
+            else:
+                print(f"Namespace \"{namespace.metadata.name}\" is not older than {days}")
+        else:
+            print(f"Namespace \"{namespace.metadata.name}\" doesn't match the Regex {name_regex}")
+
+def main():
+    parser = argparse.ArgumentParser(description='Delete Kubernetes namespaces older than a certain number of days with a specified name regex.')
+    parser.add_argument('days', type=int, help='Number of days to consider namespaces as old')
+    parser.add_argument('name_regex', type=str, help='Regular expression to match the namespace name')
+    args = parser.parse_args()
+
+    # Load the Kubernetes config from the default location
+    config.load_kube_config()
+
+    # Compile the regex pattern
+    name_regex = re.compile(args.name_regex)
+
+    # Delete old namespaces with the specified name regex
+    delete_old_namespaces(client, args.days, name_regex)
+
+if __name__ == "__main__":
+    main()
